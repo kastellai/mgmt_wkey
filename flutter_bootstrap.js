855741ceab4ea03e3b22ed25dc9074314dcca90c
kastellai <ilenia.castellana@gmail.com>
2025-11-15 13:27:42 +0100

    test app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3574676392"
+    serviceWorkerVersion: "2833225897"
   }
 });
