4d1a8f44013b5f177d96362d9624157156ba561a
kastellai <ilenia.castellana@gmail.com>
2026-01-24 13:28:23 +0100

    test new version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1888001127"
+    serviceWorkerVersion: "2416307949"
   }
 });
