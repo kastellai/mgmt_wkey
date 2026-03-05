c6e39c7f61cd00bc1862f568417576be75e59714
kastellai <ilenia.castellana@gmail.com>
2026-03-05 21:18:50 +0100

    test color unsafe area

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e6f8705c5c8f94456c55c8cf7ea6a614",
+const RESOURCES = {"flutter_bootstrap.js": "61fee3292d61f1f251c0ea4ed2448e80",
 "version.json": "9f9c34dcc25b571ea0c75552694df2ee",
 "index.html": "f79c039e0dee5631529a98490eaf5534",
 "/": "f79c039e0dee5631529a98490eaf5534",
-"main.dart.js": "c81e8e9c7dee070355d892d208d360f6",
+"main.dart.js": "27cf06de6b73fd16f81da766fdba8523",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
